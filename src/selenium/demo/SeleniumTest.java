a0f391335270da3ad4f12fee65f1ba7109700280
Anshu Pandey <kanshupandey@gmail.com>
2022-12-21 22:18:15 +0530

    some local changes

--- src/selenium/demo/SeleniumTest.java
+++ src/selenium/demo/SeleniumTest.java
@@ -1,6 +1,7 @@
 package selenium.demo;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -17,73 +18,116 @@ public class SeleniumTest {
 		// TODO Auto-generated method stub
 
 		System.setProperty("webdriver.edge.driver", "D:\\BrowserDriver\\edgedriver_win64\\msedgedriver.exe");
-		
+
 		WebDriver driver = new EdgeDriver();
 		driver.get("https://test.esgconvene.com");
-		
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(9));
-		
+
+		driver.manage().window().maximize();
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+
 		driver.findElement(By.id("username")).sendKeys("test_cso");
 		driver.findElement(By.name("password")).sendKeys("P@ssw0rd");
-		
+
 		driver.findElement(By.id("login-button")).click();
-		
+
 		// Accessing data collection tab
 		driver.findElement(By.id("convene-data")).click();
-		
-		// Accessing Energy data collection 
-		wait.until(ExpectedConditions.visibilityOfElementLocated
-				(By.cssSelector(".dashboard-pages.hide-in-mobile div:nth-child(2)")));
+
+		// Accessing Energy data collection
+		wait.until(ExpectedConditions
+				.visibilityOfElementLocated(By.cssSelector(".dashboard-pages.hide-in-mobile div:nth-child(2)")));
 		driver.findElement(By.cssSelector(".dashboard-pages.hide-in-mobile div:nth-child(2)")).click();
-		
+
 		// Accessing the new Energy data collection form
-		wait.until(ExpectedConditions.visibilityOfElementLocated
-				(By.cssSelector("#gridbox-Energy8V-content div button:nth-child(1)")));
+		wait.until(ExpectedConditions
+				.visibilityOfElementLocated(By.cssSelector("#gridbox-Energy8V-content div button:nth-child(1)")));
 		driver.findElement(By.cssSelector("#gridbox-Energy8V-content div button:nth-child(1)")).click();
-		
+
 		// Selecting a scope and inputing value to Electricity total field.
-//		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("electricity.total")));
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("location")));
 		WebElement scope = driver.findElement(By.id("location"));
 		Select location = new Select(scope);
-		
+
 		location.selectByVisibleText("Malaysia");
-		
+
+		// choosing a sub-location if any
+		subLocation(driver, wait);
+
+		// choosing a date
+		driver.findElement(
+				By.xpath("//div[@id='row41']/div[@class='elx-form-row-body']/div[2]/div[2]/div/div/div/div/div/i"))
+				.click();
+
+		wait.until(ExpectedConditions
+				.visibilityOfElementLocated(By.xpath("//div[@class='year-chooser arrow-chooser']/div[3]/select")));
+
+		WebElement years = driver.findElement(By.xpath("//div[@class='year-chooser arrow-chooser']/div[3]/select"));
+
+		Select year = new Select(years);
+
+		year.selectByVisibleText("2021");
+
+		WebElement months = driver.findElement(By.xpath("//div[@class='month-chooser arrow-chooser']/div[3]/select"));
+
+		Select month = new Select(months);
+
+		month.selectByVisibleText("September");
+
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='date-chooser']/table/tr/td")));
+
+		List<WebElement> days = driver.findElements(By.xpath("//div[@class='date-chooser']/table/tr/td"));
+
+		for (WebElement daySelect : days) {
+
+			if (daySelect.getText().equalsIgnoreCase("20")) {
+				daySelect.click();
+				break;
+			}
+		}
+
 		WebElement totalElectricity = driver.findElement(By.id("electricity.total"));
-		
+
 		totalValue(totalElectricity);
-		
+
 		// inputing value to heating total field.
 		driver.findElement(By.id("elx-tabId-heating")).click();
-		
+
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("heating.total")));
 		WebElement totalHeating = driver.findElement(By.id("heating.total"));
-		
+
 		totalValue(totalHeating);
-		
+
 		// submitting the form
 		driver.findElement(By.xpath("//div[@class='input-group']/button[2]")).click();
-		
+
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#btn-review-cancel")));
-		
+
 		driver.findElement(By.id("btn-review-cancel")).click();
 
-		System.out.println("Testing sample1");
-		
-		System.out.println("Testing sample2");
-		
-		System.out.println("Testing sample3");
-		
-		System.out.println("Testing sample4");
-		
 	}
-	
+
 	public static void totalValue(WebElement element) {
-		
-        String value = Keys.chord(Keys.CONTROL, "A");
-		
+
+		String value = Keys.chord(Keys.CONTROL, "A");
+
 		element.sendKeys(value, "5000");
-		
+
+	}
+
+	public static void subLocation(WebDriver driver, WebDriverWait wait) {
+
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#sublocation")));
+
+		if (driver.findElement(By.cssSelector("#sublocation")).isDisplayed()) {
+
+			WebElement subLocations = driver.findElement(By.cssSelector("#sublocation"));
+
+			Select subLocat = new Select(subLocations);
+
+			subLocat.selectByIndex(1);
+		}
+
 	}
 
 }
